ee2e0395b7bf95dedd540dd21dfa69c8a15fcec7
Gaetano Matonti <gaetanomatonti.dev@gmail.com>
2024-09-26 12:22:43 +0200

    Add Navigation Stack Structure (#2)

--- Colombo/Sources/CoordinatorProtocol/NavigationCoordinator.swift
+++ Colombo/Sources/CoordinatorProtocol/NavigationCoordinator.swift
@@ -3,20 +3,26 @@ import SwiftUI
 /// A protocol that defines requirements for an object that coordinates flow navigation.
 @Observable
 open class NavigationCoordinator<Router>: PresentationCoordinator where Router: Colombo.Router {
-
   public typealias Route = Router.Route
+  
+  // MARK: - Stored Properties
+  
+  /// The path of the navigation stack. Use this property as a binding for the `NavigationStack`.
+  internal(set) public var path: Stack<Route> {
+    didSet {
+      print(path)
+    }
+  }
 
   /// The root of the navigation.
   private(set) public var root: Route
 
-  /// The path of the navigation stack. Use this property as a binding for the `NavigationStack`.
-  internal(set) public var path: NavigationPath
-
+  /// The router that defines the destinations of the navigation.
   private(set) public var router: Router
 
   // MARK: - Init
 
-  public init(root: Route, path: NavigationPath = NavigationPath(), router: Router) {
+  public init(root: Route, path: Stack<Route> = [], router: Router) {
     self.root = root
     self.path = path
     self.router = router
@@ -24,18 +30,24 @@ open class NavigationCoordinator<Router>: PresentationCoordinator where Router: 
 
   // MARK: - Functions
 
-  /// Pushes the specified route into the `NavigationStack`.
+  /// Pushes the specified route into the `path`.
   public func push(_ route: Route) {
-    path.append(route)
+    path.push(route)
   }
 
-  /// Pops the last route off the `NavigationStack`.
+  /// Pops the last route off the `path`.
   public func pop() {
-    path.removeLast()
+    path.pop()
   }
 
-  /// Pops to the root of the `NavigationStack`.
+  /// Pops the last `k` elements of the `path`.
+  /// - Parameter k: The number of elements to pop off the stack.
+  public func pop(_ k: Int) {
+    path.pop(k)
+  }
+
+  /// Pops to the root of the `path`.
   public func popToRoot() {
-    path.removeLast(path.count)
+    path.popAll()
   }
 }
